19c66a81edc721933c910def7f7803fdf94fcc8c
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-08-30 11:36:55 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# 05a_Overriding_Levels
+
+### Link (SuperMario):
+https://youtu.be/cN6PmWnLjRE?t=253
+
---
+++ src/com/cc/java/App.java
@@ -0,0 +1,12 @@
+package com.cc.java;
+public class App {
+    public static void main(String[] args) {
+      // to do
+    
+
+    }
+
+    private static void ausgabe(String outStr) {
+        System.out.println(outStr);
+    }
+}
