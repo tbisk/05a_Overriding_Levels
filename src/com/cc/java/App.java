0a5dc05461096bac12314fd5b54afee83e7e2ef7
Peter <schmidt.multimedia@gmail.com>
2021-08-30 12:09:06 +0200

    Level_0 / drawio ++

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,8 +1,9 @@
 package com.cc.java;
 public class App {
     public static void main(String[] args) {
-      // to do
-    
+      
+     Level_0 player = new Level_0();
+     ausgabe(player.play());  
 
     }
 
